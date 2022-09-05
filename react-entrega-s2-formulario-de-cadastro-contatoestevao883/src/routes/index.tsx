5d15a096893ee052040d5973847f1f09eee33895
unknown <contatoestevao883@hotmail.com>
2022-09-05 17:30:08 -0300

    Adicionando proteção de rotas

--- react-entrega-s2-formulario-de-cadastro-contatoestevao883/src/routes/index.tsx
+++ react-entrega-s2-formulario-de-cadastro-contatoestevao883/src/routes/index.tsx
@@ -2,12 +2,15 @@ import  { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import Register from '../pages/Register';
+import ProtectedRoutes from '../components/ProtectedRoutes';
 
 const RoutesMain = () => (
             <Routes>
                 <Route  path = '/' element={<Login />}/>
                 <Route  path = '/register' element={<Register />}/>
-                <Route  path = '/home' element={<Home />}/>
+                <Route element={<ProtectedRoutes />}>
+                    <Route  path = '/home' element={<Home />}/>
+                </Route>
             </Routes>
 );
 
